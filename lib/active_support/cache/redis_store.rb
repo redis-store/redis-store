dcf0ae2d55963ea2dc875d9d36d3abfe4db7128e
Luca Guidi <guidi.luca@gmail.com>
2009-04-11 14:04:27 +0200

    Implemented #delete, #exist?, #increment, #decrement

--- lib/active_support/cache/redis_store.rb
+++ lib/active_support/cache/redis_store.rb
@@ -14,6 +14,68 @@ module ActiveSupport
         super
         @data.get key, options
       end
+
+      def delete(key, options = nil)
+        super
+        @data.delete key
+      end
+
+      def exist?(key, options = nil)
+        super
+        @data.key? key
+      end
+
+      # Increment a key in the store.
+      #
+      # If the key doesn't exist it will be initialized on 0.
+      # If the key exist but it isn't a Fixnum it will be initialized on 0.
+      #
+      # Example:
+      #   We have two objects in cache:
+      #     counter # => 23
+      #     rabbit  # => #<Rabbit:0x5eee6c>
+      #
+      #   cache.increment "counter"
+      #   cache.read "counter", :raw => true      # => "24"
+      #
+      #   cache.increment "counter", 6
+      #   cache.read "counter", :raw => true      # => "30"
+      #
+      #   cache.increment "a counter"
+      #   cache.read "a counter", :raw => true    # => "1"
+      #
+      #   cache.increment "rabbit"
+      #   cache.read "rabbit", :raw => true       # => "1"
+      def increment(key, options = nil)
+        log "increment", key, options
+        @data.incr key, options
+      end
+
+      # Decrement a key in the store
+      #
+      # If the key doesn't exist it will be initialized on 0.
+      # If the key exist but it isn't a Fixnum it will be initialized on 0.
+      #
+      # Example:
+      #   We have two objects in cache:
+      #     counter # => 23
+      #     rabbit  # => #<Rabbit:0x5eee6c>
+      #
+      #   cache.decrement "counter"
+      #   cache.read "counter", :raw => true      # => "22"
+      #
+      #   cache.decrement "counter", 2
+      #   cache.read "counter", :raw => true      # => "20"
+      #
+      #   cache.decrement "a counter"
+      #   cache.read "a counter", :raw => true    # => "-1"
+      #
+      #   cache.decrement "rabbit"
+      #   cache.read "rabbit", :raw => true       # => "-1"
+      def decrement(key, options = nil)
+        log "decrement", key, options
+        @data.decr key, options
+      end
     end
   end
 end
--- spec/active_support/cache/redis_store_spec.rb
+++ spec/active_support/cache/redis_store_spec.rb
@@ -7,25 +7,58 @@ module ActiveSupport
         @store  = RedisStore.new
         @rabbit = OpenStruct.new :name => "bunny"
         @white_rabbit = OpenStruct.new :color => "white"
-        @store.write "rabbit", @rabbit
+        @store.write  "rabbit", @rabbit
+        @store.delete "counter"
       end
 
       it "should read the data" do
         @store.read("rabbit").should === @rabbit
       end
-      
+
       it "should write the data" do
         @store.write "rabbit", @white_rabbit
         @store.read("rabbit").should === @white_rabbit
       end
-      
+
       it "should read raw data" do
         @store.read("rabbit", :raw => true).should == "\004\bU:\017OpenStruct{\006:\tname\"\nbunny"
       end
-      
+
       it "should write raw data" do
         @store.write "rabbit", @white_rabbit, :raw => true
         @store.read("rabbit", :raw => true).should == %(#<OpenStruct color="white">)
+      end
+
+      it "should delete data" do
+        @store.delete "rabbit"
+        @store.read("rabbit").should be_nil
+      end
+
+      it "should verify existence of an object in the store" do
+        @store.exist?("rabbit").should be_true
+        @store.exist?("rab-a-dub").should be_false
+      end
+
+      it "should increment a key" do
+        3.times { @store.increment "counter" }
+        @store.read("counter", :raw => true).to_i.should == 3
+      end
+
+      it "should decrement a key" do
+        3.times { @store.increment "counter" }
+        2.times { @store.decrement "counter" }
+        @store.read("counter", :raw => true).to_i.should == 1
+      end
+
+      it "should increment a key by given value" do
+        @store.increment "counter", 3
+        @store.read("counter", :raw => true).to_i.should == 3
+      end
+
+      it "should decrement a key by given value" do
+        3.times { @store.increment "counter" }
+        @store.decrement "counter", 2
+        @store.read("counter", :raw => true).to_i.should == 1
       end
     end
   end
