ef298fc0e158e146076a5fd2d427bc40b2735568
Luca Guidi <guidi.luca@gmail.com>
2009-04-30 10:32:58 +0200

    Hack for Merb cyclic dependency

--- lib/redis-store.rb
+++ lib/redis-store.rb
@@ -4,6 +4,8 @@ require "redis/marshaled_redis"
 require "redis/distributed_marshaled_redis"
 
 if defined?(Merb)
+  # HACK for cyclic dependency: redis-store is required before merb-cache
+  module Merb; module Cache; class AbstractStore; end end end
   require "cache/merb/redis_store"
 else # rails or ruby application
   require "activesupport"
