67783ca5c1e9f84fd26219a43b0570aa15eb3dc4
Luca Guidi <guidi.luca@gmail.com>
2010-12-15 15:35:57 +0100

    Bump version v1.0.0.beta4

--- lib/redis/store/version.rb
+++ lib/redis/store/version.rb
@@ -4,7 +4,7 @@ class Redis
       MAJOR = 1
       MINOR = 0
       TINY  = 0
-      BUILD = "beta3"
+      BUILD = "beta4"
 
       STRING = [MAJOR, MINOR, TINY, BUILD].join('.')
     end
--- spec/redis/store/version_spec.rb
+++ spec/redis/store/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe Redis::Store::VERSION do
   it "should describe Redis::Store version" do
-    Redis::Store::VERSION::STRING.should == "1.0.0.beta3"
+    Redis::Store::VERSION::STRING.should == "1.0.0.beta4"
   end
 end
