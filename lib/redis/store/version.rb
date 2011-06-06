2abaf495d515cf996ebb9901e7dd66ede2b198a8
Luca Guidi <guidi.luca@gmail.com>
2011-06-06 11:14:58 +0200

    Gemfile and gemspec are now using Bundler

--- lib/redis/store/version.rb
+++ lib/redis/store/version.rb
@@ -1,12 +1,5 @@
 class Redis
   class Store < self
-    module VERSION #:nodoc:
-      MAJOR = 1
-      MINOR = 0
-      TINY  = 0
-      BUILD = "rc1"
-
-      STRING = [MAJOR, MINOR, TINY, BUILD].join('.')
-    end
+    VERSION = '1.0.0.rc1'
   end
 end
