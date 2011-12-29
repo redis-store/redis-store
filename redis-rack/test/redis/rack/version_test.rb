93e960799ad8041f346bea5784495934ae05292e
Luca Guidi <guidi.luca@gmail.com>
2011-12-29 12:44:37 +0100

    Target Rack 1.4.0 for redis-rack

--- redis-rack/test/redis/rack/version_test.rb
+++ redis-rack/test/redis/rack/version_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Redis::Rack::VERSION do
-  it "must be equal to 1.3.0" do
-    Redis::Rack::VERSION.must_equal '1.3.0'
+  it "must be equal to 1.4.0" do
+    Redis::Rack::VERSION.must_equal '1.4.0'
   end
 end
