d5f8404ff2dcfe70dfcd254899a6bb316420d5cc
Matthew Horan <matthew.horan@livestream.com>
2012-02-01 22:36:00 -0500

    Integrate against a distributed Redis store

--- redis-actionpack/test/dummy/config/initializers/session_store.rb
+++ redis-actionpack/test/dummy/config/initializers/session_store.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 Dummy::Application.config.session_store :redis_store,
-  :key => '_session_id'
+  :key => '_session_id',
+  :redis_server => ["redis://127.0.0.1:6380/1/theplaylist",
+    "redis://127.0.0.1:6381/1/theplaylist"]
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
