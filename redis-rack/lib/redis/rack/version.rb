65f07d44dd594cc2aaf1805ebb35c8385f2f7385
Matthew Horan <matthew.horan@livestream.com>
2012-02-10 23:56:38 -0500

    Bumped:
    * redis-rack 1.4.0.rc2

--- redis-rack/lib/redis/rack/version.rb
+++ redis-rack/lib/redis/rack/version.rb
@@ -1,6 +1,6 @@
 class Redis
   module Rack
-    VERSION = '1.3.5.rc'
+    VERSION = '1.4.0.rc2'
   end
 end
 
--- redis-rack/test/redis/rack/version_test.rb
+++ redis-rack/test/redis/rack/version_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Redis::Rack::VERSION do
-  it "must be equal to 1.3.5.rc" do
-    Redis::Rack::VERSION.must_equal '1.3.5.rc'
+  it "must be equal to 1.4.0.rc2" do
+    Redis::Rack::VERSION.must_equal '1.4.0.rc2'
   end
 end
