c6178c91c222711e55e77b78f99b2ec2a3da63e4
Matthew Horan <matthew.horan@livestream.com>
2012-02-11 00:29:55 -0500

    Bumped:
    * redis-rails 3.2.1.rc

--- redis-rails/test/redis-rails/version_test.rb
+++ redis-rails/test/redis-rails/version_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Redis::Rails::VERSION do
-  it "must be equal to 3.1.3.rc4" do
-    Redis::Rails::VERSION.must_equal '3.1.3.rc4'
+  it "must be equal to 3.2.1.rc" do
+    Redis::Rails::VERSION.must_equal '3.2.1.rc'
   end
 end
