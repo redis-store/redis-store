6da180447632bb0ce1643b606052a6c0c14e4e6d
Luca Guidi <guidi.luca@gmail.com>
2012-02-14 12:16:29 +0100

    Bump redis-sinatra-1.3.2

--- redis-sinatra/lib/redis-sinatra/version.rb
+++ redis-sinatra/lib/redis-sinatra/version.rb
@@ -1,5 +1,5 @@
 class Redis
   module Sinatra
-    VERSION = '1.3.1'
+    VERSION = '1.3.2'
   end
 end
--- redis-sinatra/test/redis/sinatra/version_test.rb
+++ redis-sinatra/test/redis/sinatra/version_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Redis::Sinatra::VERSION do
-  it "must be equal to 1.3.1" do
-    Redis::Sinatra::VERSION.must_equal '1.3.1'
+  it "must be equal to 1.3.2" do
+    Redis::Sinatra::VERSION.must_equal '1.3.2'
   end
 end
