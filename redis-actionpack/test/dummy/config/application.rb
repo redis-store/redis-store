b9ce6bf6727bfb64afb96d6e1f43556ae5faaf88
Matthew Horan <matthew.horan@livestream.com>
2012-02-22 23:41:23 -0500

    Move dummy app environment config to application.rb

--- redis-actionpack/test/dummy/config/application.rb
+++ redis-actionpack/test/dummy/config/application.rb
@@ -6,6 +6,24 @@ Bundler.require
 
 module Dummy
   class Application < Rails::Application
+    # Disable class caching for session auto-load test
+    config.cache_classes = false
+
+    # Log error messages when you accidentally call methods on nil
+    config.whiny_nils = true
+
+    # Show full error reports and disable caching
+    config.consider_all_requests_local       = true
+    config.action_controller.perform_caching = false
+
+    # Raise exceptions instead of rendering exception templates
+    config.action_dispatch.show_exceptions = false
+
+    # Disable request forgery protection in test environment
+    config.action_controller.allow_forgery_protection    = false
+
+    # Print deprecation notices to the stderr
+    config.active_support.deprecation = :stderr
   end
 end
 
