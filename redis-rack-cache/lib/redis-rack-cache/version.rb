f74e47fbe0d5c9508a092a86b705956e3cb6bcd8
Matthew Horan <matt@matthoran.com>
2012-08-09 19:25:44 -0400

    Bump redis-rack-cache

--- redis-rack-cache/lib/redis-rack-cache/version.rb
+++ redis-rack-cache/lib/redis-rack-cache/version.rb
@@ -1,7 +1,7 @@
 class Redis
   module Rack
     module Cache
-      VERSION = '1.2'
+      VERSION = '1.2.1'
     end
   end
 end
--- redis-rack-cache/test/redis-rack-cache/version_test.rb
+++ redis-rack-cache/test/redis-rack-cache/version_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 describe Redis::Rack::Cache::VERSION do
-  it "must be equal to 1.2" do
-    Redis::Rack::Cache::VERSION.must_equal '1.2'
+  it "must be equal to 1.2.1" do
+    Redis::Rack::Cache::VERSION.must_equal '1.2.1'
   end
 end
